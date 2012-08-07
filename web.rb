9e6836742192c1757918d80f3f03a7734d3dcd7d
Ryan Brainard <brainard@heroku.com>
2012-08-06 17:46:36 -0700

    first pass at promote

--- web.rb
+++ web.rb
@@ -26,6 +26,10 @@ class Heroku::Client
     }) { |k, v1, v2| v1 || v2 }.merge(options)
     releases_create(app_name, payload)
   end
+
+  def release_slug(app_name)
+    json_decode(get("/apps/#{app_name}/release_slug").to_s)
+  end
 end
 
 helpers do
@@ -53,13 +57,44 @@ end
 post "/apps/:app/release" do
   api_key = creds[1]
 
+  halt(403, "must specify cloud") unless params[:cloud]
   halt(403, "must specify build_url") unless params[:build_url]
   halt(403, "must specify description") unless params[:description]
 
-  release = Dir.mktmpdir do |dir|
-    escaped_build_url = Shellwords.escape(params[:build_url])
+  release_from_url(api_key, params[:cloud], params[:app], params[:build_url], params[:description],params[:processes])
+end
 
-    if params[:build_url] =~ /\.tgz$/
+post "/apps/:app/promote" do
+  api_key = creds[1]
+
+  halt(403, "must specify cloud") unless params[:cloud]
+
+  downstream_app = downstream_app(api_key)
+
+  downstream_slug = api(api_key, params[:cloud]).release_slug(params[:app])
+  release_from_url(api_key, params[:cloud], downstream_app, downstream_slug["slug_url"], "Promotion from #{params[:app]} #{downstream_slug["name"]}", nil)
+end
+
+private
+
+def downstream_app(api_key)
+  if params.has_key? :DOWNSTREAM_APP
+    return params[:DOWNSTREAM_APP]
+  end
+  
+  config_vars = api(api_key, params[:cloud]).config_vars(params[:app])
+  if config_vars.has_key? "DOWNSTREAM_APP"
+    return config_vars["DOWNSTREAM_APP"]
+  end
+
+  halt(403, "unknown DOWNSTREAM_APP. either set as config var on upstream app or as query param")
+end
+
+def release_from_url(api_key, cloud, app, build_url, description, processes)
+  release = Dir.mktmpdir do |dir|
+    escaped_build_url = Shellwords.escape(build_url)
+
+    if build_url =~ /\.tgz$/
       %x{ mkdir -p #{dir}/tarball }
       %x{ cd #{dir}/tarball && curl #{escaped_build_url} -s -o- | tar xzf - }
       %x{ mksquashfs #{dir}/tarball #{dir}/squash -all-root }
@@ -70,8 +105,8 @@ post "/apps/:app/release" do
 
     %x{ unsquashfs -d #{dir}/extract #{dir}/build Procfile }
 
-    if params[:processes]
-      procfile = params[:processes]
+    if processes
+      procfile = processes
     else
       if File.exists?("#{dir}/extract/Procfile")
         procfile = File.read("#{dir}/extract/Procfile").split("\n").inject({}) do |ax, line|
@@ -82,13 +117,13 @@ post "/apps/:app/release" do
     end
 
     release_options = {
-      "process_types" => procfile
+        "process_types" => procfile
     }
 
-    release = api(api_key, params[:cloud]).release(params[:app], "#{dir}/build", params[:description], release_options)
+    release = api(api_key, cloud).release(app, "#{dir}/build", description, release_options)
     release["release"]
   end
 
   content_type "application/json"
-  JSON.dump({ "release" => release })
+  JSON.dump({"release" => release})
 end
