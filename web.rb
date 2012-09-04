9d56c7679231ad1b7dbe9162589409d36c2ddaaf
David Dollar <ddollar@gmail.com>
2012-09-04 15:23:52 -0400

    fix default host

--- web.rb
+++ web.rb
@@ -29,7 +29,7 @@ class Heroku::Client
 end
 
 helpers do
-  def api(key, cloud="api.heroku.com")
+  def api(key, cloud="heroku.com")
     client = Heroku::Client.new("david@heroku.com", key)
     client.host = cloud
     client
